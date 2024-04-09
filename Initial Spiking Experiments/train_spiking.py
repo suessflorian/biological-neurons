8efb6fbe3512bf9806a97f2d80f6a45875052009
ClassicMMT <porlovskiy@hotmail.com>
2024-04-09 12:02:08 +1200

    Reorganising repo

--- Initial Spiking Experiments/train_spiking.py
+++ Initial Spiking Experiments/train_spiking.py
@@ -8,38 +8,41 @@ from torch import autograd
 
 from spiking_modules import spikingNeuron
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda' if torch.cuda.is_available(
+) else 'mps' if torch.backends.mps.is_available() else 'cpu')
 
 num_classes = 10
 num_epochs = 50
 batch_size = 100
 learning_rate = 0.01
 
-input_size = 28*28 #per row
+input_size = 28*28  # per row
 t = 4
 
-# MNIST dataset 
-train_dataset = torchvision.datasets.MNIST(root='./data', 
-                                           train=True, 
-                                           transform=transforms.ToTensor(),  
+# MNIST dataset
+train_dataset = torchvision.datasets.MNIST(root='./data',
+                                           train=True,
+                                           transform=transforms.ToTensor(),
                                            download=True)
 
-test_dataset = torchvision.datasets.MNIST(root='./data', 
-                                          train=False, 
+test_dataset = torchvision.datasets.MNIST(root='./data',
+                                          train=False,
                                           transform=transforms.ToTensor())
 
 # Data loader
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
-                                           batch_size=batch_size, 
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
                                            shuffle=True)
 
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
-                                          batch_size=batch_size, 
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
                                           shuffle=False)
 
-## Create feed forward layer
+# Create feed forward layer
+
+
 class SpikingFeedForward(nn.Module):
-    def __init__(self, in_features: int, out_features: int, device = None, dtype = None):
+    def __init__(self, in_features: int, out_features: int, device=None, dtype=None):
         super().__init__()
         self.l1 = spikingNeuron(in_features, 1024, device)
         self.l2 = spikingNeuron(1024, 512, device)
@@ -49,28 +52,29 @@ class SpikingFeedForward(nn.Module):
         init_x = x
 
         for i in range(t):
-            #with torch.autograd.detect_anomaly():
+            # with torch.autograd.detect_anomaly():
             x = self.l1(init_x, i)
             x = self.l2(x, i)
             x = self.l3(x, i)
 
         return x
 
-## Training method
+
+# Training method
 model = SpikingFeedForward(input_size, num_classes, device)
 
 criterion = nn.CrossEntropyLoss()
-optim = torch.optim.SGD(model.parameters(), lr = 0.01)
+optim = torch.optim.SGD(model.parameters(), lr=0.01)
 
 # Train the model
 n_total_steps = len(train_loader)
 for epoch in range(num_epochs):
-    for i, (images, labels) in enumerate(train_loader):  
+    for i, (images, labels) in enumerate(train_loader):
         # origin shape: [N, 1, 28*28]
         # resized: [N, 28*28]
         images = images.reshape(-1, input_size).to(device)
         labels = labels.to(device)
-        
+
         # Forward pass
         outputs = model(images, t)
         loss = criterion(outputs, labels)
@@ -79,9 +83,10 @@ for epoch in range(num_epochs):
         optim.zero_grad()
         loss.backward()
         optim.step()
-        
+
         if (i+1) % 100 == 0:
-            print (f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
+            print(
+                f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
 
 # Test the model
 # In test phase, we don't need to compute gradients (for memory efficiency)
