f4cd06f1bd5472492804815c9463a03c6cc20868
ClassicMMT <porlovskiy@hotmail.com>
2024-04-10 22:50:56 +1200

    Baseline Models

---
+++ Baseline Models/run.py
@@ -0,0 +1,65 @@
+import torch
+import torchvision
+from models import LeNet5
+from scripts import train_model, test_model
+
+##### Options #####
+
+device = torch.device('mps' if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu')
+load_name = 'CIFAR-10-LeNet5-50-epochs' # set to None if loading not required
+save_name = 'CIFAR-10-LeNet5-50-epochs' # set to None if saving not required
+train = True
+
+
+##### Hyperparameters #####
+
+batch_size = 256
+learning_rate = 0.01
+n_epochs = 50
+
+
+##### Data #####
+
+transforms = torchvision.transforms.Compose([
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize((0,0,0), (1,1,1))
+])
+
+train_dataset = torchvision.datasets.CIFAR10('data', train = True, transform=transforms)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_dataset = torchvision.datasets.CIFAR10('data', train = False, transform=transforms)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+
+##### Model #####
+
+model = LeNet5().to(device)
+
+if load_name:
+    try:
+        model.load_state_dict(torch.load('Baseline Models/models/' + load_name + '.pt'))
+    except:
+        print('Model Not Found. Using Untrained Model.')
+
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+##### Training #####
+
+if train:
+    model, results = train_model(model, 
+                                loader=train_loader, 
+                                optimizer=optimizer, 
+                                n_epochs=n_epochs, 
+                                device=device)
+
+
+##### Evaluation #####
+
+accuracy = test_model(model, loader=test_loader, device=device)
+print(f'Test Accuracy: {accuracy*100:.4f}%')
+
+
+##### Saving #####
+
+if save_name:
+    torch.save(model.state_dict(), 'Baseline Models/models/' + save_name + '.pt')
---
+++ Baseline Models/scripts.py
@@ -0,0 +1,60 @@
+import torch
+from utils import printf
+
+def train_model(model, loader, optimizer, n_epochs, device):
+    print('\n---------- Training ----------\n')
+    criterion = torch.nn.CrossEntropyLoss()
+
+    train_loss, train_acc, test_acc = [], [], []
+
+    for epoch in range(n_epochs):
+        model.train()
+        epoch_loss = 0
+        n_correct, n_total = 0, 0
+        for i, (features, labels) in enumerate(loader):
+            features, labels = features.to(device), labels.to(device)
+            
+            output = model(features)
+            loss = criterion(output, labels)
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            epoch_loss += loss.item()
+            correct = (output.argmax(dim = -1) == labels).sum().item()
+            n_correct += correct
+            n_total += len(labels)
+            
+            if (i + 1) % 20 == 0:
+                printf(f'Epoch: [{epoch+1}/{n_epochs}], Batch: [{i+1}/{len(loader)}], Loss: {loss.item():.4f}, Training Accuracy: {correct/len(labels):.4f}')
+        
+        epoch_loss /= len(loader)    
+        
+        train_loss.append(epoch_loss)
+        train_acc.append(n_correct / n_total)
+    
+    print() # for prettiness
+    
+    results = {
+        'epochs': torch.arange(1, n_epochs + 1),
+        'train losses': train_loss,
+        'train accuracies': train_acc
+    }
+    
+    return model, results
+
+def test_model(model, loader, device):
+    print('\n---------- Testing ----------\n')
+    with torch.no_grad():
+        model.eval()
+        n_correct, n_total = 0, 0
+        
+        for features, labels in loader:
+            features, labels = features.to(device), labels.to(device)
+            output = model(features)
+            output = output.argmax(dim=-1)
+            n_correct += (output == labels).sum().item()
+            n_total += len(labels)
+    
+        return n_correct / n_total
