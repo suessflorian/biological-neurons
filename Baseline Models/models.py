b1134a5e5c3c7667f9d7f33c2c3e94301114ad1d
ClassicMMT <porlovskiy@hotmail.com>
2024-04-12 15:10:27 +1200

    Fashion Models

--- Baseline Models/models.py
+++ Baseline Models/models.py
@@ -1,10 +1,9 @@
 import torch.nn.functional as F
 import torch.nn as nn
 
-class LeNet5(nn.Module):
-    # CIFAR-10
+class LeNet5_CIFAR(nn.Module):
     def __init__(self):
-        super(LeNet5, self).__init__()
+        super(LeNet5_CIFAR, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, kernel_size=5, stride=1, padding=0)
         self.bn1 = nn.BatchNorm2d(6)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
@@ -28,4 +27,30 @@ class LeNet5(nn.Module):
         x = F.relu(x)
         x = self.fc3(x)
         return x
+    
+class LeNet5_MNIST(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(6)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5, stride=1, padding=0)
+        self.bn2 = nn.BatchNorm2d(16)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120) 
+        self.bn3 = nn.BatchNorm1d(120)
+        self.fc2 = nn.Linear(120, 84)
+        self.bn4 = nn.BatchNorm1d(84)
+        self.fc3 = nn.Linear(84, 10)
 
+    def forward(self, x):
+        x = self.bn1(self.conv1(x))
+        x = self.pool(F.relu(x))
+        x = self.bn2(self.conv2(x))
+        x = self.pool(F.relu(x))
+        x = x.view(-1, 16 * 4 * 4)  # Flatten the tensor for the fully connected layer
+        x = self.bn3(self.fc1(x))
+        x = F.relu(x)
+        x = self.bn4(self.fc2(x))
+        x = F.relu(x)
+        x = self.fc3(x)
+        return x
